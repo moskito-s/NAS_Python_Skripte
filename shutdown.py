e3a1e5e00e15f5265ce9becf03d8b9a66fce0208
Leo Fiedler <leo.fiedler@kit.edu>
2021-10-05 17:31:44 +0200

    taster implementiert

--- shutdown.py
+++ shutdown.py
@@ -1,2 +1,13 @@
 import os
-os.system('sudo shutdown -r now')
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+
+# input of the switch will change the state of the LED
+while True:
+    if GPIO.input(26):
+        os.system('sudo reboot')
