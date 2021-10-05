b74397ce9e1016cf17ac76dce6fb4f8e969aab44
Leo Fiedler <leo.fiedler@kit.edu>
2021-10-05 17:15:04 +0200

    added test file

---
+++ blink_test.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+
+state = True
+
+# endless loop, on/off for 1 second
+while True:
+        GPIO.output(21,True)
+        time.sleep(1)
+        GPIO.output(21,False)
+        time.sleep(1)
