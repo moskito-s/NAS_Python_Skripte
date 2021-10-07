d430b4cb79e8e3f1ca515b6ac00cb996097107c3
Leo Fiedler <github@info-fiedler.de>
2021-10-07 22:26:14 +0200

    blinken

--- hdd_busy_blink.py
+++ hdd_busy_blink.py
@@ -2,11 +2,27 @@ import RPi.GPIO as GPIO
 import time
 import psutil
 
-import psutil
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
 
-obj_Disk = psutil.disk_usage('/media/nextCloudMainHDD')
+state = True
 
-print (obj_Disk.total)
-print (obj_Disk.used)
-print (obj_Disk.free)
-print (obj_Disk.percent)
+
+
+obj_Disk_old = psutil.disk_usage('/media/nextCloudMainHDD')
+obj_Disk_new = psutil.disk_usage('/media/nextCloudMainHDD')
+
+
+# endless loop, on/off for 1 second
+while True:
+
+    obj_Disk_new = psutil.disk_usage('/media/nextCloudMainHDD')
+
+    if obj_Disk_new.used != obj_Disk_new.used :
+        GPIO.output(21,True)
+        time.sleep(0.4)
+        GPIO.output(21,False)
+        time.sleep(0.4)
+
+    obj_Disk_old = obj_Disk_new
