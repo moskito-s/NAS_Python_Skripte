2f84d8e52c4ecd06c8f7ab06e0aade98849b4515
Leo Fiedler <github@info-fiedler.de>
2021-11-23 23:24:28 +0100

    added file

---
+++ frontpanel_function.py
@@ -0,0 +1,63 @@
+import RPi.GPIO as GPIO
+import os
+import time
+import psutil
+import threading as th
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(26,GPIO.IN)
+
+obj_Disk_old = psutil.disk_usage('/media/USBdrive/ncdata')
+obj_Disk_new = psutil.disk_usage('/media/USBdrive/ncdata')
+
+def buttonPress(channel):
+    global buttonStatus
+    start_time = time.time()
+
+    while GPIO.input(channel) == 0: # Wait for the button up
+        pass
+
+    buttonTime = time.time() - start_time    # How long was the button down?
+
+    if .1 <= buttonTime < 2:
+        print("reboot")       
+        os.system('sudo reboot')         
+    elif 2 <= buttonTime :
+        print("shutdown")
+        GPIO.output(21,False)
+        time.sleep(0.4)
+        GPIO.output(21,True)
+        time.sleep(0.4)          
+        os.system('sudo shutdown now') 
+
+def checkForHDDChange():
+
+    if obj_Disk_new.used != obj_Disk_old.used :
+        GPIO.output(21,True)
+        time.sleep(0.4)
+        GPIO.output(21,False)
+        time.sleep(0.4)
+
+    obj_Disk_old = obj_Disk_new
+
+
+GPIO.output(20,True) #turn on main power light and pull up for switch
+GPIO.add_event_detect(26, GPIO.FALLING, callback=buttonPress, bouncetime=500)
+
+
+
+
+
+def main():
+    print("Frontpanel aktiv")
+    while(True):
+        pass
+
+if __name__ == "__main__":
+    main()
+
+
+            
