23be0895d2124c9d085776b129136ead4f62bfa8
Leo Fiedler <github@info-fiedler.de>
2021-11-25 19:19:04 +0100

    assigend pins

--- frontpanel_function.py
+++ frontpanel_function.py
@@ -4,11 +4,16 @@ import time
 import psutil
 from threading import Timer
 
+
+pin_sw2 = 26
+pin_d3 = 19
+pin_d1 = 16
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
-GPIO.setup(26, GPIO.IN)
+GPIO.setup(pin_d3, GPIO.OUT)
+GPIO.setup(pin_d1, GPIO.OUT)
+GPIO.setup(pin_sw2, GPIO.IN)
 
 
 
@@ -22,6 +27,7 @@ obj_Disk_new = psutil.disk_usage('/media/USBdrive/ncdata')
 
 def buttonPress(channel):
     global buttonStatus
+    global obj_Disk_new, obj_Disk_old, pin_d1, pin_d3, pin_d1
     start_time = time.time()
 
     while GPIO.input(channel) == 0: # Wait for the button up
@@ -34,37 +40,38 @@ def buttonPress(channel):
         os.system('sudo reboot')         
     elif 2 <= buttonTime :
         print("shutdown")
-        GPIO.output(21,False)
+        GPIO.output(pin_d1,False)
         time.sleep(0.4)
-        GPIO.output(21,True)
+        GPIO.output(pin_d1,True)
         time.sleep(0.4)          
         os.system('sudo shutdown now') 
 
 def checkForHDDChange():
-    global obj_Disk_new, obj_Disk_old
+    global obj_Disk_new, obj_Disk_old, pin_d1, pin_d3, pin_d1
 
     obj_Disk_new = psutil.disk_usage('/media/USBdrive/ncdata')
 
 
     if obj_Disk_new.used != obj_Disk_old.used :
         print("HDD changed")
-        GPIO.output(21,True)
+        GPIO.output(pin_d3,True)
         time.sleep(0.4)
-        GPIO.output(21,False)
+        GPIO.output(pin_d3,False)
         time.sleep(0.4)
         obj_Disk_old = obj_Disk_new
 
 
 
 
-GPIO.output(20,True) #turn on main power light and pull up for switch
-GPIO.add_event_detect(26, GPIO.FALLING, callback=buttonPress, bouncetime=500)
+GPIO.output(pin_d1,True) #turn on main power light and pull up for switch
+GPIO.add_event_detect(pin_sw2, GPIO.FALLING, callback=buttonPress, bouncetime=500)
 
 
 
 
 
 def main():
+    global obj_Disk_new, obj_Disk_old, pin_d1, pin_d3, pin_d1
     print("Frontpanel aktiv")
     hddCheckTimer = RepeatTimer(1, checkForHDDChange)
     hddCheckTimer.start()
