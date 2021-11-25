956044ae6b264fbc9fb74a566782b08bbf6ad91a
Leo Fiedler <github@info-fiedler.de>
2021-11-25 19:39:15 +0100

    fix

--- frontpanel_function.py
+++ frontpanel_function.py
@@ -22,8 +22,9 @@ class RepeatTimer(Timer):
         while not self.finished.wait(self.interval):
             self.function(*self.args, **self.kwargs)
 
-obj_Disk_old = psutil.disk_usage('/media/USBdrive/ncdata')
-obj_Disk_new = psutil.disk_usage('/media/USBdrive/ncdata')
+obj_Disk_old_size = 0
+obj_Disk_new_size = 0
+
 
 def buttonPress(channel):
     global buttonStatus
@@ -47,18 +48,25 @@ def buttonPress(channel):
         os.system('sudo shutdown now') 
 
 def checkForHDDChange():
-    global obj_Disk_new, obj_Disk_old, pin_d1, pin_d3, pin_d1
+    global obj_Disk_old_size, obj_Disk_new_size, pin_d1, pin_d3, pin_d1
 
-    obj_Disk_new = psutil.disk_usage('/media/USBdrive/ncdata')
+    try:
 
+        obj_Disk_new = psutil.disk_usage('/media/USBdrive/ncdata')
 
-    if obj_Disk_new.used != obj_Disk_old.used :
+    except Exception:
+        GPIO.output(pin_d3,False)
+        time.sleep(0.2)
+        GPIO.output(pin_d3,True)
+
+    obj_Disk_new_size = obj_Disk_new.used
+
+    if obj_Disk_new_size != obj_Disk_old_size :
         print("HDD changed")
         GPIO.output(pin_d3,True)
         time.sleep(0.4)
         GPIO.output(pin_d3,False)
-        time.sleep(0.4)
-        obj_Disk_old = obj_Disk_new
+        obj_Disk_old_size = obj_Disk_new_size
 
 
 
@@ -70,7 +78,7 @@ def main():
     global obj_Disk_new, obj_Disk_old, pin_d1, pin_d3, pin_d1
     print("Frontpanel aktiv")
     GPIO.output(pin_d1,True) #turn on main power light and pull up for switch
-    hddCheckTimer = RepeatTimer(1, checkForHDDChange)
+    hddCheckTimer = RepeatTimer(0.8, checkForHDDChange)
     hddCheckTimer.start()
 
 
